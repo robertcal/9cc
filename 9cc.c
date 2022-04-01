748bece1c11363c1ed18373087d79bc9fadd39a2
robertcal <robertcal900@gmail.com>
2022-04-01 21:05:34 +0900

    加減算のできるコンパイラを実装

--- 9cc.c
+++ 9cc.c
@@ -12,10 +12,23 @@ int main(int argc, char **argv) {
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10)); // 10進数でポインタから数値に変換できるところを変換して、ポインタを進める
 
     while (*p) { // 文字列が終わるまで（文字列の最後はヌル文字が入りwhile文が終了する）
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
 
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です： '%c'\n", *p);
+        return 1;
     }
 
     printf("  ret\n");
