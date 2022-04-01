6c94df2419ae2b1ed73b2201fcbe31881d1be6fa
robertcal <robertcal900@gmail.com>
2022-04-01 15:41:17 +0900

    加減算のできるコンパイラの途中まで

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,17 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1]; // 引数の文字列の先頭ポインタ
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
     printf("  mov rax, %d\n", atoi(argv[1]));
+
+    while (*p) { // 文字列が終わるまで（文字列の最後はヌル文字が入りwhile文が終了する）
+
+    }
+
     printf("  ret\n");
     return 0;
 }
