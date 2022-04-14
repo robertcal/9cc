29825049b2c27053151b4321cd7d2b66f42da2e3
robertcal <robertcal900@gmail.com>
2022-04-14 21:45:22 +0900

    ノードを作る関数まで追加

--- 9cc.c
+++ 9cc.c
@@ -7,6 +7,42 @@
 // 入力
 char *user_input;
 
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs; // 左辺
+    Node *rhs; // 右辺
+    int val; // kindがND_NUMの場合のみ使う
+};
+
+// 新しいノードを作成する
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node)); // ノードはどこまで増えるか分からないため、動的にメモリを確保する必要がある
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// 新しい整数ノードを作成する
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node)); // ノードはどこまで増えるか分からないため、動的にメモリを確保する必要がある
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 // トークンの種類
 typedef enum {
     TK_RESERVED, // 記号
